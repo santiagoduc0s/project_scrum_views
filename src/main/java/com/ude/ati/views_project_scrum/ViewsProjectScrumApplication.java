b57e924c909cf4e3cd8ba8f3abbb32a63024ca07
Santiago Ducos <santidu200@gmail.com>
2021-07-31 20:04:28 -0300

    primer commit

---
+++ src/main/java/com/ude/ati/views_project_scrum/ViewsProjectScrumApplication.java
@@ -0,0 +1,13 @@
+package com.ude.ati.views_project_scrum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViewsProjectScrumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ViewsProjectScrumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ude/ati/views_project_scrum/ViewsProjectScrumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ude.ati.views_project_scrum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViewsProjectScrumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
