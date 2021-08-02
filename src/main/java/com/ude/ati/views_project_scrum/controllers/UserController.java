00369917ed28c1e119a8f607a041e16a9bfe553b
unknown <gabriel8476@hotmail.com>
2021-08-01 22:37:25 -0300

    Se comenzó a crear la vista de registro usuario

---
+++ src/main/java/com/ude/ati/views_project_scrum/controllers/UserController.java
@@ -0,0 +1,19 @@
+package com.ude.ati.views_project_scrum.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+//    public String editProfile(){
+//
+//        return "modificarUsuario";
+//    }
+    @GetMapping("/registro")
+    public String register(){
+
+        return "user/register";
+    }
+
+}
