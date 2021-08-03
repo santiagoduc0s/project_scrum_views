3b451868f94ab0ffc0eac9de179be5dea9d5af1b
unknown <gabriel8476@hotmail.com>
2021-08-02 21:39:41 -0300

    Se creó la vista login

---
+++ src/main/java/com/ude/ati/views_project_scrum/controllers/AuthController.java
@@ -0,0 +1,12 @@
+package com.ude.ati.views_project_scrum.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthController {
+    @GetMapping("login")
+    public String login() {
+        return "auth/login";
+    }
+}
